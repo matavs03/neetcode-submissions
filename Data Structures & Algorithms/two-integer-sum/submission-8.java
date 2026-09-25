1ec47a1aeccf34dd0cb3324d2d3a504f5990a55d
Matija Veljković - Šupić <166246057+matavs03@users.noreply.github.com>
2026-09-25 22:48:12 +0200

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mapa = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int razlika = target - nums[i];
+            if(mapa.containsKey(razlika)) return new int[]{mapa.get(razlika), i};
+            else mapa.put(nums[i], i); 
+        }
+        return new int[0];
+    }
+}
